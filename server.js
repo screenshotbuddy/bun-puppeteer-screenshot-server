3525b5b8a4f9a60dde7c55e459bec843e43bacfb
screenshotbuddy <admin@screenshotbuddy.com>
2025-04-01 00:52:28 +0530

    created bun server with native routes

--- server.js
+++ server.js
@@ -1,51 +1,55 @@
 import puppeteer from "puppeteer"
 const server = Bun.serve({
-  async fetch(req) {
-    const { pathname: path, searchParams } = new URL(req.url)
+  port: 3000,
+  routes: {
+    // creating a bun server endpoint
+    "/screenshot": {
+      GET: async (req) => {
+        // parsing request URL
+        const { searchParams } = new URL(req.url)
 
-    if (
-      req.method === "GET" &&
-      path === "/screenshot" &&
-      searchParams.get("url")
-    ) {
-      const url = searchParams.get("url")
+        if (searchParams.get("url")) {
+          // geting url query param from request URL
+          const url = searchParams.get("url")
 
-      console.log("url :>> ", url)
-      let browser
-      try {
-        // Launch a Chromium browser instance
-        browser = await puppeteer.launch({
-          headless: true, // running with headless mode
-          args: ["--no-sandbox", "--disable-setuid-sandbox"], // minimal arguments required to start headless browser
-        })
+          let browser
+          try {
+            // Launch a Chromium browser instance
+            browser = await puppeteer.launch({
+              headless: true, // running with headless mode
+              args: ["--no-sandbox", "--disable-setuid-sandbox"], // minimal arguments required to start headless browser
+            })
 
-        // Create a new page in the browser
-        const page = await browser.newPage()
+            // Create a new page in the browser
+            const page = await browser.newPage()
 
-        // Navigate to the specified URL
-        await page.goto(url)
+            // Navigate to the specified URL
+            await page.goto(url)
 
-        // Take a screenshot of the current page you can set type 'png' | 'jpeg' | 'webp'
-        const buffer = await page.screenshot({ type: "png" })
+            // Take a screenshot of the current page you can set type 'png' | 'jpeg' | 'webp'
+            // you wil need to change Content-Type if you change screenshot type here accordingly
+            const buffer = await page.screenshot({ type: "png" })
 
-        // Set the content type for the response set correct header based on type you set above
-        const headers = new Headers()
-        headers.append("Content-Type", "image/png")
-        headers.append("Access-Control-Allow-Origin", "*")
+            // Seting the content type for the response
+            // set correct header based on screenshot type you set above
+            const headers = new Headers()
+            headers.append("Content-Type", "image/png")
+            headers.append("Access-Control-Allow-Origin", "*")
 
-        // Send the screenshot as a response
-        return new Response(buffer, { headers })
-      } catch (error) {
-        console.error("Error taking screenshot:", error)
-        return new Response("Internal Server Error", { status: 500 })
-      } finally {
-        // Close the browser instance
-        await browser.close()
-      }
-    }
-
-    // 404
-    return new Response("Page not found", { status: 404 })
+            // Send the screenshot as a response
+            return new Response(buffer, { headers })
+          } catch (error) {
+            console.error("Error taking screenshot:", error)
+            return new Response("Internal Server Error", { status: 500 })
+          } finally {
+            // Close the browser instance
+            await browser.close()
+          }
+        } else {
+          return new Response("Invalid request", { status: 500 })
+        }
+      },
+    },
   },
 })
 
---
+++ server_old.js
@@ -0,0 +1,52 @@
+import puppeteer from "puppeteer"
+const server = Bun.serve({
+  async fetch(req) {
+    const { pathname: path, searchParams } = new URL(req.url)
+
+    if (
+      req.method === "GET" &&
+      path === "/screenshot" &&
+      searchParams.get("url")
+    ) {
+      const url = searchParams.get("url")
+
+      console.log("url :>> ", url)
+      let browser
+      try {
+        // Launch a Chromium browser instance
+        browser = await puppeteer.launch({
+          headless: true, // running with headless mode
+          args: ["--no-sandbox", "--disable-setuid-sandbox"], // minimal arguments required to start headless browser
+        })
+
+        // Create a new page in the browser
+        const page = await browser.newPage()
+
+        // Navigate to the specified URL
+        await page.goto(url)
+
+        // Take a screenshot of the current page you can set type 'png' | 'jpeg' | 'webp'
+        const buffer = await page.screenshot({ type: "png" })
+
+        // Set the content type for the response set correct header based on type you set above
+        const headers = new Headers()
+        headers.append("Content-Type", "image/png")
+        headers.append("Access-Control-Allow-Origin", "*")
+
+        // Send the screenshot as a response
+        return new Response(buffer, { headers })
+      } catch (error) {
+        console.error("Error taking screenshot:", error)
+        return new Response("Internal Server Error", { status: 500 })
+      } finally {
+        // Close the browser instance
+        await browser.close()
+      }
+    }
+
+    // 404
+    return new Response("Page not found", { status: 404 })
+  },
+})
+
+console.log(`Listening on ${server.url}`)
